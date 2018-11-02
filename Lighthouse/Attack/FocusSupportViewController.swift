4ab1efa695ae9aae0c475e19aba039a9a59170ed
Alina Yu <alinayu8@yahoo.com>
2018-11-02 16:59:18 -0400

    Added constraints

--- Lighthouse/Attack/FocusSupportViewController.swift
+++ Lighthouse/Attack/FocusSupportViewController.swift
@@ -1,25 +1,25 @@
 //
-//  ViewController.swift
+//  FocusSupportViewController.swift
 //  Lighthouse
 //
-//  Created by Alina Yu on 10/29/18.
+//  Created by Alina Yu on 11/2/18.
 //  Copyright © 2018 Alina Yu. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class FocusSupportViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
