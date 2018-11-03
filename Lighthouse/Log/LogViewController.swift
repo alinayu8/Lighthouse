606d5d14ce0805d6ccbde2c9b60f694229746c4a
Alina Yu <alinayu8@yahoo.com>
2018-11-03 17:49:55 -0400

    Triggered switch tabs

--- Lighthouse/Log/LogViewController.swift
+++ Lighthouse/Log/LogViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class LogViewController: UIViewController {
     
+    @IBOutlet weak var listContainer: UIView!
+    @IBOutlet weak var statsContainer: UIView!
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBAction func indexChanged(_ sender: AnyObject) {
+        switch segmentedControl.selectedSegmentIndex
+        {
+        case 0:
+            listContainer.isHidden = true
+            statsContainer.isHidden = false
+        case 1:
+            listContainer.isHidden = false
+            statsContainer.isHidden = true
+        default:
+            break
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
