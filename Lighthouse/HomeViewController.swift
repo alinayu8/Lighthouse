03113abb934e37cf492da2880031519ba1294921
Alina Yu <alinayu8@yahoo.com>
2018-11-03 21:52:05 -0400

    Set start time for entry when button is pressed

--- Lighthouse/HomeViewController.swift
+++ Lighthouse/HomeViewController.swift
@@ -7,9 +7,42 @@
 //
 
 import UIKit
+import CoreData
+import CoreLocation
 
 class HomeViewController: UIViewController {
     
+    // MARK: - Buttons
+    @IBAction func triggerButton(_ sender: UIButton) {
+        let entry = Entry()
+        entry.startTime = Date() //set start time of attack, time zone
+        saveEntry(entry: entry)
+        print(entry.startTime)
+    }
+    
+    
+    // MARK: - CoreData functions
+    func saveEntry(entry: Entry){
+        // Connect to the context for the container stack
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        // Specifically select the Entry entity to save this object to
+        let entity = NSEntityDescription.entity(forEntityName: "Entries", in: context)
+        let newEntity = NSManagedObject(entity: entity!, insertInto: context)
+        // Set values one at a time and save
+        newEntity.setValue(entry.startTime, forKey: "start_time")
+        newEntity.setValue(entry.endTime, forKey: "end_time")
+        newEntity.setValue(entry.latitude, forKey: "latitude")
+        newEntity.setValue(entry.longitude, forKey: "longitude")
+        newEntity.setValue(entry.notes, forKey: "notes")
+        do {
+            try context.save()
+        } catch {
+            print("Failed saving")
+        }
+    }
+    
+    // MARK: - General
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
---
+++ Lighthouse/Models/Entry.swift
@@ -0,0 +1,32 @@
+//
+//  Entry.swift
+//  Lighthouse
+//
+//  Created by Alina Yu on 11/3/18.
+//  Copyright © 2018 Alina Yu. All rights reserved.
+//
+
+import Foundation
+
+class Entry: NSObject {
+    var startTime: Date = Date()
+    var endTime: Date?
+    var latitude: Float?
+    var longitude: Float?
+    var notes: String?
+    
+    override init() {
+        super.init()
+    }
+    
+    init(startTime: Date, endTime: Date, latitude: Float, longitude: Float, notes: String) {
+        self.startTime = startTime
+        self.endTime = endTime
+        self.latitude = latitude
+        self.longitude = longitude
+        self.notes = notes
+        super.init()
+    }
+}
+
+
