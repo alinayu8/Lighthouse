96f395e52bbdbb9b3e91c945e9e0f4c131635e65
Shirley Zhou <shirleyzhou7@gmail.com>
2018-11-29 11:14:27 -0500

    added diff quotes change on button press

--- Lighthouse/Attack Controllers/TrackButtonsViewController.swift
+++ Lighthouse/Attack Controllers/TrackButtonsViewController.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 import CoreData
-let qodURL: NSURL = NSURL(string: "https://quotes.rest/qod?category=inspire")!
-let data = NSData(contentsOf: qodURL as URL)!
+//let qodURL: NSURL = NSURL(string: "https://quotes.rest/qod?category=inspire")!
+//let data = NSData(contentsOf: qodURL as URL)!
+
 class TrackButtonsViewController: UIViewController {
   
-    // MARK: - Quote
+    // MARK: - Quote outlet
   @IBOutlet weak var quoteLabel: UILabel?
-  @IBOutlet weak var authorLabel: UILabel?
+
     // MARK: - Buttons 
     
     @IBAction func stopEntryButton(_ sender: UIButton) {
@@ -26,6 +27,7 @@ class TrackButtonsViewController: UIViewController {
         datapoint.time = Date() //set start time of attack, time zone
         datapoint.value = 1
         addDatapoint(datapoint: datapoint)
+        randQuote()
     }
     
     @IBAction func worseButton(_ sender: UIButton) {
@@ -33,6 +35,7 @@ class TrackButtonsViewController: UIViewController {
         datapoint.time = Date() //set start time of attack, time zone
         datapoint.value = -1
         addDatapoint(datapoint: datapoint)
+        randQuote()
     }
     
     // MARK: - CoreData functions
@@ -98,46 +101,17 @@ class TrackButtonsViewController: UIViewController {
             print("Failed")
         }
     }
+  let quotes = ["It is during our darkest moments that we must focus to see the light. \n -Aristotle",
+                "I can't change the direction of the wind, but I can adjust my sails to always reach my destination. \n -Jimmy Dean",
+                "I will love the light for it shows me the way, yet I will endure the darkness because it shows me the stars. \n -Og Mandino",
+                "Our greatest glory is not in never falling, but in rising every time we fall. \n -Confucius",
+                "Promise me you'll always remember: You're braver than you believe, and stronger than you seem, and smarter than you think. \n -Christopher Robin"
+  ]
   
-  
-  func pullQuote() {
-    
-    var results = [String]()
-    
-    do {
-      let json = try JSONSerialization.jsonObject(with: data as Data, options: .allowFragments) as! [String:AnyObject]
-      if let contents = json["contents"] as? NSDictionary {
-        
-        print(contents)
-        if let quoteDatas = contents["quotes"] as? [NSDictionary] {
-          for quoteData in quoteDatas{
-            print(quoteData)
-            for kvpair in quoteData {
-              if let left = kvpair.0 as? String {
-                if left == "quote" {
-                  if let right = kvpair.1 as? String {
-                    results.append (right)
-                  }
-                }
-                if left == "author" {
-                  if let right = kvpair.1 as? String {
-                    results.append (right)
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-      
-    }catch {
-      print("error serializing JSON: \(error)")
-    }
-    let size = results.count
-    if size == 2 {
-      authorLabel?.text = results[0]
-      quoteLabel?.text = results[1]
-    }
+  //pick a random element from array of quotes
+  func randQuote() {
+    let random = quotes.randomElement()
+    quoteLabel?.text = random
   }
   
   
@@ -151,7 +125,8 @@ class TrackButtonsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        pullQuote()
+        randQuote()
+      
     }
     
     override func didReceiveMemoryWarning() {
